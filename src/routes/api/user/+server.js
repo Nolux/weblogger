4e74c5f13d14e9f93c653d225702b4e5d5fdf4ec
Nolux <Atle.selbek@gmail.com>
2024-04-25 16:00:31 +0200

    added user API

---
+++ src/routes/api/user/+server.js
@@ -0,0 +1,51 @@
+import { json, error } from "@sveltejs/kit";
+import { db } from "$lib/db.js";
+import { checkIfAdmin, checkIfOwner } from "$lib/server/auth.js";
+
+export const DELETE = async ({ request, locals }) => {
+  const { userId } = await request.json();
+
+  if (!userId) {
+    return error(400, "Missing input");
+  }
+
+  console.log(userId, locals.user.id);
+  console.log(!checkIfAdmin(locals));
+  console.log(checkIfOwner(userId, locals));
+  // Check if admin
+  if (!checkIfAdmin(locals)) {
+    if (!checkIfOwner(userId, locals)) {
+      return error(401, "Missing auth");
+    }
+  }
+
+  // Get project from db
+  const user = await db.user.findUnique({
+    where: { id: userId },
+  });
+
+  console.log(user);
+
+  // Remove all connections to current projects
+  const removeConnectedProjects = await db.user.update({
+    where: { id: userId },
+    data: {
+      assignedProjects: {
+        disconnect: [
+          ...user.projectIds.map((projectId) => {
+            return { id: projectId };
+          }),
+        ],
+      },
+    },
+  });
+
+  // TODO: remove logs?
+
+  // Finally remove Project
+  const deletedUser = await db.user.delete({
+    where: { id: userId },
+  });
+
+  return json(deletedUser);
+};
