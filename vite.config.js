dfb3ece87ac553f3dbe5cd1b25ecc4264fba8865
Nolux <Atle.selbek@gmail.com>
2024-04-27 18:41:02 +0200

    added sockets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { eventEmitter } from "/src/lib/event.js";
+
+import { Server } from "socket.io";
+
+const webSocketServer = {
+  name: "websocket",
+  configureServer(server) {
+    if (!server.httpServer) return;
+
+    const io = new Server(server.httpServer);
+
+    io.on("connection", (socket) => {
+      console.log("connected: ", socket.id);
+      socket.emit("eventFromServer", "✅ Connected");
+      eventEmitter.on("newLog", () => {
+        console.log("Helloooo! first listener");
+        socket.emit("eventFromServer", "event");
+      });
+    });
+  },
+};
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), webSocketServer],
 });
