389be253277cad889f3adeaa9468d6b549434afd
Nolux <Atle.selbek@gmail.com>
2024-05-01 02:25:30 +0200

    Helper for editor colors

---
+++ src/lib/helpers/editColors.js
@@ -0,0 +1,50 @@
+export const editColors = {
+  blue: {
+    avid: "blue",
+    ppro: "",
+    davinci: "",
+    fcpx: "",
+  },
+  cyan: {
+    avid: "cyan",
+    ppro: "4292277273",
+    davinci: "",
+    fcpx: "",
+  },
+  green: {
+    avid: "green",
+    ppro: "",
+    davinci: "",
+    fcpx: "",
+  },
+  yellow: {
+    avid: "yellow",
+    ppro: "4289825711",
+    davinci: "",
+    fcpx: "",
+  },
+  red: {
+    avid: "red",
+    ppro: "4281740498",
+    davinci: "",
+    fcpx: "",
+  },
+  pink: {
+    avid: "magenta",
+    ppro: "",
+    davinci: "",
+    fcpx: "",
+  },
+  black: {
+    avid: "black",
+    ppro: "",
+    davinci: "",
+    fcpx: "",
+  },
+  white: {
+    avid: "",
+    ppro: "4294741314",
+    davinci: "",
+    fcpx: "",
+  },
+};
