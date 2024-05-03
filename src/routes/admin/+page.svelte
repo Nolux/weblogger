1e1dc654eb562a58f5c339cbf6018089575b6f14
Nolux <Atle.selbek@gmail.com>
2024-05-04 00:43:07 +0200

    admin basepage

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,6 @@
-admin
+<div class="flex flex-col gap-8">
+  <h1 class="text-3xl font-bold text-center hidden lg:block lg:text-left">
+    Admin
+  </h1>
+  <div class="flex flex-col gap-4 w-full justify-between"></div>
+</div>
