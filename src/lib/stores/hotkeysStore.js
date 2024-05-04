8efee8531fb370e312e5ef8f1790c30c4275e008
Nolux <Atle.selbek@gmail.com>
2024-05-05 00:37:04 +0200

    changed default hotkeys

--- src/lib/stores/hotkeysStore.js
+++ src/lib/stores/hotkeysStore.js
@@ -5,49 +5,49 @@ import { persisted } from "svelte-persisted-store";
 export const hotkeys = persisted("hotkeys", [
   {
     key: "F1",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F2",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F3",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F4",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F5",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F6",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F7",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
   {
     key: "F8",
-    modifiers: { control: true, shift: false, alt: false },
+    modifiers: { control: false, shift: false, alt: false },
   },
 ]);
 
 export const submitHotkey = persisted("submitHotkey", {
   key: "F1",
-  modifiers: { control: true, shift: true, alt: false },
+  modifiers: { control: false, shift: true, alt: false },
 });
 
 export const resetHotkey = persisted("resetHotkey", {
   key: "F2",
-  modifiers: { control: true, shift: true, alt: false },
+  modifiers: { control: false, shift: true, alt: false },
 });
 
 export const timecodeHotkey = persisted("timecodeHotkey", {
   key: "F3",
-  modifiers: { control: true, shift: true, alt: false },
+  modifiers: { control: false, shift: true, alt: false },
 });
