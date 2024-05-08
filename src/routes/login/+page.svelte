b574d0c5bbf08a808e2958067219e8fb91c35005
Nolux <Atle.selbek@gmail.com>
2024-05-09 01:31:44 +0200

    added spinner to login sceen

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,10 +4,22 @@
   import { enhance } from "$app/forms";
 
   export let form;
+
+  let loading = false;
 </script>
 
 <section>
-  <form method="post" class="flex flex-col gap-4" use:enhance>
+  <form
+    method="post"
+    class="flex flex-col gap-4"
+    use:enhance={() => {
+      loading = true;
+      return async ({ update }) => {
+        await update();
+        loading = false;
+      };
+    }}
+  >
     <h1 class="text-3xl">Login</h1>
     <label for="email" class="input input-bordered flex items-center gap-2">
       <Icon icon="mdi-light:email" width="24" height="24" style="fill-current"
@@ -35,7 +47,10 @@
     </label>
 
     <div class="submit-container">
-      <button type="submit" class="btn btn-primary w-full">Login</button>
+      <button type="submit" class="btn btn-primary w-full"
+        >{#if loading}<span class="loading loading-spinner loading-sm"
+          ></span>{:else}Login{/if}</button
+      >
     </div>
 
     {#if form?.error}
