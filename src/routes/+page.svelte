d666f08f51264c37c4d414e960dbe6a250a244e1
Nolux <Atle.selbek@gmail.com>
2024-05-09 01:18:06 +0200

    Updated landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,8 @@
 </script>
 
 <div class="flex flex-col gap-8">
-  <h1 class="text-3xl font-bold text-center hidden lg:block lg:text-left">
-    {user
-      ? `Welcome back ${user.firstName} to Weblogger`
-      : "Welcome to Weblogger"}
+  <h1 class="text-3xl font-bold text-center lg:text-left">
+    {user ? `Welcome back ${user.firstName}!` : "Welcome to Weblogger"}
   </h1>
   {#if user}
     <h2 class="text-xl text-center">Select module:</h2>
@@ -29,6 +27,16 @@
       </a>
     </div>
   {:else}
-    <h2 class="text-xl">Sign in using the sidebar or menu top left</h2>
+    <div class="flex flex-col lg:flex-row justify-around items-center gap-4">
+      <a href="/login" class="text-center flex-1">
+        <div class="text-xl font-bold">LOGIN</div>
+        <div>Sign in to get access to your project</div>
+      </a>
+      <div class="divider divider-horizontal"></div>
+      <a href="/about" class="text-center flex-1">
+        <div class="text-xl font-bold">ABOUT</div>
+        <div>Who made this?!</div>
+      </a>
+    </div>
   {/if}
 </div>
