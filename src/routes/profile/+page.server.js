0c32e540b8968890cedd7297c38190582e923940
Nolux <Atle.selbek@gmail.com>
2024-05-10 00:45:07 +0200

    added the option to change password in userProfile

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 import { db } from "$lib/db.js";
 
 export async function load({ fetch, locals }) {
@@ -6,7 +8,7 @@ export async function load({ fetch, locals }) {
 }
 
 export const actions = {
-  default: async ({ request, locals }) => {
+  changeUser: async ({ request, locals }) => {
     const data = await request.formData();
 
     const userId = locals.user.id;
@@ -32,5 +34,37 @@ export const actions = {
 
     console.log(userId, email, firstName, lastName);
     return;
+  },
+  changePassword: async ({ request, locals }) => {
+    const data = await request.formData();
+
+    const userId = locals.user.id;
+
+    const password = data.get("password");
+    const oldPassword = data.get("oldPassword");
+
+    const user = await db.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      return {
+        error: "No user",
+      };
+    }
+
+    // Verify the password
+    const passwordIsValid = await bcrypt.compare(oldPassword, user.password);
+
+    if (!passwordIsValid) {
+      return {
+        error: "Old password is not valid",
+      };
+    }
+
+    const updatedUser = await db.user.update({
+      where: { id: userId },
+      data: { password: await bcrypt.hash(password, 10) },
+    });
+
+    return { success: "Password changed" };
   },
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,17 +3,27 @@
   import { enhance } from "$app/forms";
 
   $: user = data.user;
-  $: console.log(user);
+  let passwordInput = { password: "", passwordConfirm: "" };
+
+  export let form;
 </script>
 
 <h1 class="text-3xl font-bold text-center hidden lg:block lg:text-left">
   Profile
 </h1>
 {#if user}
-  <div class="grid gap-4">
+  {#if form?.error}
+    {form.error}
+  {/if}
+  {#if form?.success}
+    {form.success}
+  {/if}
+  <div class="grid gap-4 md:w-1/2 xl:w-1/4 lg:m-auto">
+    <div class="text-xl">Edit User</div>
     <form
       method="POST"
-      class="flex flex-col gap-4 md:w-1/2 xl:w-1/4 lg:m-auto"
+      action="?/changeUser"
+      class="flex flex-col gap-4"
       use:enhance
     >
       <label class="flex gap-2 items-center justify-between">
@@ -44,6 +54,47 @@
         /></label
       >
       <button class="btn">Save</button>
+    </form>
+    <div class="divider"></div>
+    <div class="text-xl">Change Password</div>
+
+    <form
+      method="POST"
+      action="?/changePassword"
+      class="flex flex-col gap-4"
+      use:enhance
+    >
+      <label class="flex gap-2 items-center justify-between">
+        Old password:
+        <input
+          type="password"
+          name="oldPassword"
+          class="input input-bordered"
+        /></label
+      >
+      <label class="flex gap-2 items-center justify-between">
+        New password:
+        <input
+          type="password"
+          name="password"
+          class="input input-bordered"
+          bind:value={passwordInput.password}
+        /></label
+      >
+      <label class="flex gap-2 items-center justify-between">
+        Confirm new password:
+        <input
+          type="password"
+          name="password"
+          class="input input-bordered"
+          bind:value={passwordInput.passwordConfirm}
+        /></label
+      >
+      <button
+        class="btn w-full"
+        disabled={passwordInput.password !== passwordInput.passwordConfirm ||
+          passwordInput.password == ""}>Change password</button
+      >
     </form>
   </div>
 {/if}
